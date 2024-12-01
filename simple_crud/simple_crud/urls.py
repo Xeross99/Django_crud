d697987d4413355e37e918eb94ee52f104095b30
Michał Krzysteczko <m.krzysteczko@icloud.com>
2024-12-01 23:41:04 +0100

    wip

--- simple_crud/simple_crud/urls.py
+++ simple_crud/simple_crud/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("__reload__/", include("django_browser_reload.urls")),
     path('', views.index, name='index'),
+    path('wszyskie_produkty', views.products, name='index'),
 ]
