b8fb7bf3994f198225fe2f8f0fdb683029e4146f
Michał Krzysteczko <m.krzysteczko@icloud.com>
2025-01-18 17:47:19 +0100

    Add .gitignore and update product URL path in urlpatterns

--- simple_crud/simple_crud/urls.py
+++ simple_crud/simple_crud/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path("__reload__/", include("django_browser_reload.urls")),
     path('', views.index, name='root_path'),
     path('wszystkie_produkty/', views.products, name='products_path'),
-    path('produkty/<int:product_id>/', views.product_id, name='product_path'),
     path('produkty/nowy/', views.create_product, name='product_new_path'),
+    path('produkt/<int:product_id>/', views.product_id, name='product_path'),
     path('produkt/<int:product_id>/edytuj/', views.edit_product, name='product_edit_path'),
 ]
